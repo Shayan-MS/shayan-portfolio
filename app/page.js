81e74e858cec5f5e81c4a74eb5d15bc65a749898
Shayan-MS <mr.msryns@gmail.com>
2025-12-10 13:42:45 +0330

    Fix Skills Section

--- app/page.js
+++ app/page.js
@@ -15,9 +15,20 @@ import Link from "next/link";
 import { LuArrowLeft } from "react-icons/lu";
 import { TypingText } from "@/src/components/lightswind/typing-text";
 import Header from "./components/home/Header";
-import ScrollFloat from "@/components/ScrollFloat";
 import ScrollStack from "@/src/components/lightswind/scroll-stack";
 import ClickSpark from "@/components/ClickSpark";
+
+import { InteractiveCard } from "@/src/components/lightswind/interactive-card";
+import {
+  Brackets,
+  Check,
+  CodeXml,
+  Layers,
+  PanelsTopLeft,
+  PenTool,
+  Ruler,
+  Shapes,
+} from "lucide-react";
 
 const Home = () => {
   const containerRef = useRef(null);
@@ -96,7 +107,7 @@ const Home = () => {
                       letterSpacing="tracking-wider"
                       align="center"
                     >
-                      Hey, I'm Shayan
+                      Hi, I'm Shayan
                     </TypingText>
                   </h1>
                   <div className="relative z-0 text-2xl sm:text-5xl text-blue-500">
@@ -259,6 +270,138 @@ const Home = () => {
               </div>
             </div>
           </section>
+          {/* Skills */}
+          <section className="skills-bg lg:h-[40%]" id="skills">
+            <div className="flex flex-col lg:flex-row-reverse lg:items-center lg:justify-center xl:mx-32 2xl:mx-56 h-full py-40">
+              {/* <div className="flex flex-col items-center justify-center">
+                <div
+                  className="
+      relative 
+      rounded-xl
+      p-1
+      border-[3px] border-[#0043ff]
+      shadow-[0_0_40px_-5px_rgba(0,67,255,1)]
+      bg-transparent  
+  "
+                >
+                  <img
+                    src="/img/shayan-skills.png"
+                    alt="Shayan Profile"
+                    className="w-[370px] h-[370px] rounded-lg object-cover"
+                  />
+                </div>
+              </div> */}
+              <div className="flex flex-col items-center">
+                <h2 className="text-[#4073ff] text-5xl lg:text-6xl font-bold text-center font-serif my-10 lg:my-0">
+                  SKILLS
+                  <div className="flex justify-center mt-5">
+                    <hr className="border-b-3 w-[150px] color-blue" />
+                  </div>
+                </h2>
+                <div className="lg:mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                  <InteractiveCard
+                    InteractiveColor="#0043ff"
+                    className="text-white border-2 border-[#f172ff] rounded-xl p-7 font-semibold text-lg"
+                  >
+                    <div className="text-[#f172ff]">
+                      <Shapes size={80} />
+                      <p className="text-2xl ms-2 mt-2">UI UX DESIGN</p>
+                      <div className="ms-2 mt-3 flex flex-col space-y-1">
+                        <span className="text-[16px] flex items-center gap-x-0.5">
+                          <Check size={17} />
+                          Interface Design
+                        </span>
+                        <span className="text-[16px] flex items-center gap-x-0.5">
+                          <Check size={17} />
+                          Experience Strategy
+                        </span>
+                        <span className="text-[16px] flex items-center gap-x-0.5">
+                          <Check size={17} />
+                          Wireframes & Prototypes
+                        </span>
+                        <span className="text-[16px] flex items-center gap-x-0.5">
+                          <Check size={17} />
+                          Design Systems
+                        </span>
+                        <span className="text-[16px] flex items-center gap-x-0.5">
+                          <Check size={17} />
+                          Responsive Design
+                        </span>
+                      </div>
+                    </div>
+                  </InteractiveCard>
+                  <InteractiveCard
+                    InteractiveColor="#0043ff"
+                    className="text-white border-2 border-[#edf500] rounded-xl p-7 font-semibold text-lg"
+                  >
+                    <div className="text-[#edf500]">
+                      <CodeXml size={80} />
+                      <p className="text-2xl ms-2 mt-2 text-nowrap">
+                        WEB DEVELOPMENT
+                      </p>
+                      <div className="ms-2 mt-3 flex flex-col space-y-1">
+                        <span className="text-[16px] flex items-center gap-x-0.5">
+                          <Check size={17} />
+                          Interface Design
+                        </span>
+                        <span className="text-[16px] flex items-center gap-x-0.5">
+                          <Check size={17} />
+                          Experience Strategy
+                        </span>
+                        <span className="text-[16px] flex items-center gap-x-0.5">
+                          <Check size={17} />
+                          Wireframes & Prototypes
+                        </span>
+                        <span className="text-[16px] flex items-center gap-x-0.5">
+                          <Check size={17} />
+                          Design Systems
+                        </span>
+                        <span className="text-[16px] flex items-center gap-x-0.5">
+                          <Check size={17} />
+                          Responsive Design
+                        </span>
+                      </div>
+                    </div>
+                  </InteractiveCard>
+                  <InteractiveCard
+                    InteractiveColor="#0043ff"
+                    className="text-white border-2 rounded-xl p-7 font-semibold text-lg"
+                  >
+                    <div className="text-white text-2xl">
+                      <div>
+                        <Layers size={80} />
+                        <p className="text-2xl ms-2 mt-2 text-wrap">
+                          WORDPRESS DEVELOPMENT
+                        </p>
+                        <div className="ms-2 mt-3 flex flex-col space-y-2">
+                          <span className="text-[16px] flex items-center gap-x-0.5">
+                            <Check size={17} />
+                            Interface Design
+                          </span>
+                          <span className="text-[16px] flex items-center gap-x-0.5">
+                            <Check size={17} />
+                            Experience Strategy
+                          </span>
+                          <span className="text-[16px] flex items-center gap-x-0.5">
+                            <Check size={17} />
+                            Wireframes & Prototypes
+                          </span>
+                          <span className="text-[16px] flex items-center gap-x-0.5">
+                            <Check size={17} />
+                            Design Systems
+                          </span>
+                          <span className="text-[16px] flex items-center gap-x-0.5">
+                            <Check size={17} />
+                            Responsive Design
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                  </InteractiveCard>
+                </div>
+              </div>
+            </div>
+          </section>
           {/* PORTFOLIO */}
           <section
             className="flex flex-col items-center justify-center bg-zinc-900 text-white"
@@ -292,84 +435,6 @@ const Home = () => {
               cardHeader={true}
               backgroundColor="black"
             />
-          </section>
-          {/* Skills */}
-          <section className="aboutMe-bg lg:h-[40%]" id="skills">
-            <div className="flex flex-col lg:flex-row lg:items-center lg:justify-between xl:mx-32 2xl:mx-56 h-full py-40">
-              <div className="flex flex-col items-center justify-center">
-                <div
-                  className="
-      relative 
-      rounded-full 
-      p-1.5
-      border-[3px] border-[#0043ff]
-      shadow-[0_0_40px_-5px_rgba(0,67,255,0.7)]
-      bg-transparent  
-  "
-                >
-                  <img
-                    src="/img/aboutMePicture.png"
-                    alt="Shayan Profile"
-                    className="w-[370px] h-[370px] rounded-full object-cover"
-                  />
-                </div>
-                <div className="mt-7">
-                  <h3 className="text-4xl text-[#0043ff] font-light tracking-wider drop-shadow-[0_0_10px_rgba(0,67,255,0.8)]">
-                    [ ShayaN ]
-                  </h3>
-                </div>
-              </div>
-              <div className="flex flex-col">
-                <h2 className="text-[#4073ff] text-5xl lg:text-6xl font-bold text-center font-serif my-10 lg:my-0">
-                  SKILLS
-                  <div className="flex justify-center mt-5">
-                    <hr className="border-b-3 w-[150px] color-blue" />
-                  </div>
-                </h2>
-                <div className="lg:mt-5">
-                  <p className="text-white text-lg lg:w-[585px] mx-5 sm:mx-10 lg:mx-0">
-                    I’m Shayan Masourians, a 20-year-old student and Full Stack
-                    Developer whose world is where the logic of code meets the
-                    art of creation. 👋 I’ve been exploring this exciting path
-                    for a year now, currently gaining experience and growing
-                    alongside the Tanil content team. What keeps me motivated
-                    isn't just writing lines of code—it's the thrill of solving
-                    complex problems. I’m passionate about building solutions
-                    that simplify life for people while crafting interfaces that
-                    are genuinely beautiful. Beyond the world of ones and zeros?
-                    You’ll likely find me recharging in nature 🌲 or immersed in
-                    the gaming world 🎮.
-                  </p>
-                </div>
-                <div className="text-white flex justify-center space-y-5 space-x-5 mt-10">
-                  <div>
-                    <Link
-                      href={"/instagram"}
-                      className="text-4xl lg:text-5xl text-[#4979ff] transition hover:transform-3d"
-                    >
-                      <FaInstagram className="fill-current" />
-                    </Link>
-                  </div>
-                  <div>
-                    <Link
-                      href={"/telegram"}
-                      className="text-4xl lg:text-5xl text-[#4979ff] transition
-"
-                    >
-                      <FaTelegram />
-                    </Link>
-                  </div>
-                  <div>
-                    <Link
-                      href={"/whatsapp"}
-                      className="text-4xl lg:text-5xl text-[#4979ff] transition"
-                    >
-                      <FaWhatsapp />
-                    </Link>
-                  </div>
-                </div>
-              </div>
-            </div>
           </section>
         </main>
       </div>
